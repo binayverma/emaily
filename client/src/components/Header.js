83fe5cccd7637435b7de7e8133f7aa6307d8dc3b
Binay Kumar Verma <binayverma01@gmail.com>
2018-08-21 02:53:52 +0530

    Developing client side - Reducers, Routes, Header

---
+++ client/src/components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component }  from 'react';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a href="#" className="left brand-logo">Emaily</a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li><a href="sass.html">Login With Google</a></li>
+          </ul>
+        </div>
+      </nav>
+    )
+  }
+}
